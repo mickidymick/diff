9bda9013b818d1452dd3ac2b836af6edef0f472f
jmcmicha <jmcmicha@com1636.eecs.utk.edu>
2023-09-13 13:23:33 -0400

    updated build

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-g++ -o diff.so diff.cpp $(yed --print-cflags) $(yed --print-ldflags) -w -fpermissive
-# g++ -o diff.so diff.cpp $(yed --print-cflags) $(yed --print-ldflags) -Wno-error -fpermissive
+g++ -o diff.so diff.cpp $(yed --print-cppflags) $(yed --print-ldflags) -w -fpermissive
